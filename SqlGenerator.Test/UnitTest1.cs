58982f2477b254a0e2ba2eb4118e5a40fcc03f55
Miguel Nieto <m.nieto@kevull.com>
2020-11-13 19:47:11 +0100

    Skeleton

---
+++ SqlGenerator.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace SqlGenerator.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
