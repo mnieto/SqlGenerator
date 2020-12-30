05580249f900f656562827414672917381af7cda
Miguel Nieto <m.nieto@kevull.com>
2020-12-30 21:37:50 +0100

    Fix test broken at commit 75d2fcfe

--- SqlGenerator.Test/SourceTest.cs
+++ SqlGenerator.Test/SourceTest.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
 
 namespace SqlGenerator.Test
 {
@@ -17,21 +18,29 @@ namespace SqlGenerator.Test
 
         [Fact]
         public void isLoaded_is_false_until_data_is_readed() {
+            var discoverFactory = new Mock<IDiscoverFieldDefFactory>();
+            discoverFactory.Setup(x => x.GetDiscoverStrategy(It.IsAny<DiscoverStrategy>()))
+                .Returns(new FieldDefParserStrategy());
+
             var settings = Options.Create(new Specification {
                 DiscoverStrategy = DiscoverStrategy.FieldDefDescriptor,
                 TableName = "MyTable"
             });
-            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, new FieldDefParserStrategy(), SampleData.FieldDescriptorData());
+            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, discoverFactory.Object, SampleData.FieldDescriptorData());
             Assert.False(sut.IsLoaded);
         }
 
         [Fact]
         public void source_with_errors_has_no_fields() {
+            var discoverFactory = new Mock<IDiscoverFieldDefFactory>();
+            discoverFactory.Setup(x => x.GetDiscoverStrategy(It.IsAny<DiscoverStrategy>()))
+                .Returns(new FieldDefParserStrategy());
+
             var settings = Options.Create(new Specification {
                 DiscoverStrategy = DiscoverStrategy.FieldDefDescriptor,
                 TableName = "MyTable"
             });
-            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, new FieldDefParserStrategy(), SampleData.ErroneousFieldDescriptorData());
+            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, discoverFactory.Object, SampleData.ErroneousFieldDescriptorData());
             sut.Load("imput filename");
             Assert.False(sut.IsLoaded);         //Source with errors will not have fields and thus, IsLoaded status is false
             Assert.Null(sut.TableDef);          //Source with errors will not have a defined TableDef
@@ -43,11 +52,15 @@ namespace SqlGenerator.Test
 
         [Fact]
         public void load_with_field_descriptor() {
+            var discoverFactory = new Mock<IDiscoverFieldDefFactory>();
+            discoverFactory.Setup(x => x.GetDiscoverStrategy(It.IsAny<DiscoverStrategy>()))
+                .Returns(new FieldDefParserStrategy());
+
             var settings = Options.Create(new Specification {
                 DiscoverStrategy = DiscoverStrategy.FieldDefDescriptor,
                 TableName ="MyTable"
             });
-            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, new FieldDefParserStrategy(), SampleData.FieldDescriptorData());
+            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, discoverFactory.Object, SampleData.FieldDescriptorData());
             sut.Load("imput filename");
             Assert.True(sut.IsLoaded);
             Assert.Equal(settings.Value.TableName, sut.TableName);
@@ -57,21 +70,29 @@ namespace SqlGenerator.Test
 
         [Fact]
         public void must_be_loaded_before_read_data() {
+            var discoverFactory = new Mock<IDiscoverFieldDefFactory>();
+            discoverFactory.Setup(x => x.GetDiscoverStrategy(It.IsAny<DiscoverStrategy>()))
+                .Returns(new FieldDefParserStrategy());
+
             var settings = Options.Create(new Specification {
                 DiscoverStrategy = DiscoverStrategy.FieldDefDescriptor,
                 TableName = "MyTable"
             });
-            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, new FieldDefParserStrategy(), SampleData.FieldDescriptorData());
+            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, discoverFactory.Object, SampleData.FieldDescriptorData());
             Assert.Throws<InvalidOperationException>(() => sut.Read());
         }
 
         [Fact]
         public void must_be_readed_before_get_data() {
+            var discoverFactory = new Mock<IDiscoverFieldDefFactory>();
+            discoverFactory.Setup(x => x.GetDiscoverStrategy(It.IsAny<DiscoverStrategy>()))
+                .Returns(new FieldDefParserStrategy());
+
             var settings = Options.Create(new Specification {
                 DiscoverStrategy = DiscoverStrategy.FieldDefDescriptor,
                 TableName = "MyTable"
             });
-            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, new FieldDefParserStrategy(), SampleData.FieldDescriptorData());
+            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, discoverFactory.Object, SampleData.FieldDescriptorData());
             sut.Load("imput filename");
             Assert.Throws<InvalidOperationException>(() => sut[0]);
         }
@@ -79,11 +100,15 @@ namespace SqlGenerator.Test
 
         [Fact]
         public void can_read_data() {
+            var discoverFactory = new Mock<IDiscoverFieldDefFactory>();
+            discoverFactory.Setup(x => x.GetDiscoverStrategy(It.IsAny<DiscoverStrategy>()))
+                .Returns(new FieldDefParserStrategy());
+
             var settings = Options.Create(new Specification {
                 DiscoverStrategy = DiscoverStrategy.FieldDefDescriptor,
                 TableName = "MyTable"
             });
-            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, new FieldDefParserStrategy(), SampleData.ReadData());
+            var sut = new TestSource(settings, NullLogger<SourceTest>.Instance, discoverFactory.Object, SampleData.ReadData());
             sut.Load("imput filename");
             bool more = sut.Read();
             Assert.True(more);
@@ -108,9 +133,9 @@ namespace SqlGenerator.Test
         private (List<string> Header, List<string[]> Data) sampleData;
         public TestSource(IOptions<Specification> options,
                                          ILogger logger,
-                                         IFieldDefStrategy dataTypeSampling,
+                                         IDiscoverFieldDefFactory discoverFactory,
                                          (List<string> Header, List<string[]> Data) sampleData) :
-            base(options, logger, dataTypeSampling) {
+            base(options, logger, discoverFactory) {
 
             this.sampleData = sampleData;
         }
